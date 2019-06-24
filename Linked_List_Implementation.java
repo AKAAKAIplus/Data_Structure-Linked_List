d4457ef144fa2158b02b379d8d8802dff442c618
Kai <twnoken@gmail.com>
2019-06-24 23:57:15 +0800

    update Linked_List_Implementation.java
    adding deleting

--- Linked_List_Implementation.java
+++ Linked_List_Implementation.java
@@ -19,19 +19,35 @@ public class Linked_List_Implementation {
 		
 		node_showing(first_node);
 		
-		
+		node_deleting(second_node);
+		node_showing(first_node);
+	}
+
+	private static void node_deleting(Node deletedNode) {
+		// TODO Auto-generated method stub
+		deletedNode.front_node.next_node = deletedNode.next_node;
+		deletedNode.next_node.front_node = deletedNode.front_node;
+		deletedNode = null;
 	}
 
 	private static void node_showing(Node node) {
 		// TODO Auto-generated method stub
-		System.out.print("\nPrint All Linked List From This Node ->");
+		System.out.print("\n\nPrint All Linked List From This Node ->");
 		System.out.println(node);
 		while(node.next_node!=null) {
 			System.out.print(node.data);
-			System.out.print(" => ");
+			System.out.print("/(F)");
+			System.out.print(node.front_node!=null? node.front_node.data:"no front node");
+			System.out.print("/(N)");
+			System.out.print(node.next_node!=null? node.next_node.data:"no next node");
+			System.out.println(" => ");
 			node = node.next_node;
 		}
-		System.out.println(node.data);
+		System.out.print(node.data);
+		System.out.print("/(F)");
+		System.out.print(node.front_node!=null? node.front_node.data:"no front node");
+		System.out.print("/(N)");
+		System.out.print(node.next_node!=null? node.next_node.data:"no next node");
 	}
 
 	private static Node node_append(Node node, Object second_content) {
@@ -51,6 +67,7 @@ public class Linked_List_Implementation {
 
 class Node {
 	Node next_node = null;
+	Node front_node = null;
 	Object data;
 	
 	public Node(Object input_data) {
@@ -60,6 +77,7 @@ class Node {
 	public Node AppendNode(Object second_content) {
 		// TODO Auto-generated method stub
 		Node new_node = new Node(second_content);
+		new_node.front_node = this;
 		System.out.print("Create the new node, the content is :");
 		System.out.println(second_content);
 		next_node = new_node;
